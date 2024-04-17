00b2f285fba6bb01f14dcb3146808b0b67877f75
Thomasperge <thomaskauffmant74@gmail.com>
2024-04-18 01:00:22 +0200

    feat: adding foryou page 2/2

--- src/components/foryou/foryouchallenge/foryouchallenge.tsx
+++ src/components/foryou/foryouchallenge/foryouchallenge.tsx
@@ -1,4 +1,5 @@
 import ForYouChallengeGenerateByComponent from "../challengegenerateby/challengegenerateby.js";
+import likesvg from "./../../../assets/svg/like.svg"
 import "./foryouchallenge.css";
 
 interface ContainerProps {
@@ -23,18 +24,26 @@ const ForYouChallengeTweetComponent: React.FC<ContainerProps> = ({ ...props }) =
 
       {/* Right Tweet */}
       <div className="for-you-tweet-right-container">
+        <div className="for-you-tweet-users-area">
+          <span style={{ fontWeight: "600" }}>{props.name}</span>
+          <span style={{ fontWeight: "400", color: "gray" }}>@{props.username} • {props.waspostedtime}</span>
+        </div>
 
+        <div style={{ fontWeight: "500" }}>{props.sentence}</div>
+
+        {/* Container challenge join */}
+        <ForYouChallengeGenerateByComponent
+          usernamehasgeneratechallenge={"thomasperge"}
+          challengetitle={"Make 1000 pushup in 30m"}
+          numberpeoplejoined={"750"}
+          challengepicture={"https://assets.gqindia.com/photos/5cee7eb00379a73d25177759/4:3/w_1440,h_1080,c_limit/Pushup.jpg"}
+        />
+
+        <div className="for-you-tweet-interaction">
+          <img className="for-you-tweet-like flex" src={likesvg} alt="" />
+          <div>{props.likes} likes</div>
+        </div>
       </div>
-
-      {/* Container challenge join */}
-      <ForYouChallengeGenerateByComponent
-        usernamehasgeneratechallenge={"thomasperge"}
-        challengetitle={"Make 1000 pushup in 30m"}
-        numberpeoplejoined={"750"}
-        challengepicture={"https://assets.gqindia.com/photos/5cee7eb00379a73d25177759/4:3/w_1440,h_1080,c_limit/Pushup.jpg"}
-      />
-
-
     </div>
   );
 };
--- src/pages/discover/discover.tsx
+++ src/pages/discover/discover.tsx
@@ -1,12 +1,111 @@
 import ForYouChallengeTweetComponent from "../../components/foryou/foryouchallenge/foryouchallenge.js";
+import './discover.css'
 
 const Discover: React.FC = () => {
+	return (
+		<div className="discover-container ion-padding-vertical">
+			<ForYouChallengeTweetComponent
+				username={"godzila2.0"}
+				name={"godliza2.0"}
+				waspostedtime={"17h"}
+				sentence={"I found this challenge from @askralos, and I killed it 🤣🙃"}
+				challengeid={"456789"}
+				likes={"458"}
+				comments={"12"}
+				profilepicture={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/512px-LEGO_logo.svg.png"}
+			></ForYouChallengeTweetComponent>
 
-  return (
-    <div className="container flex" style={{ height: "100%", color: "black", fontWeight: '600' }}>
-      <ForYouChallengeTweetComponent username={""} name={""} waspostedtime={""} sentence={""} challengeid={""} likes={""} comments={""} profilepicture={""}></ForYouChallengeTweetComponent>
-    </div>
-  );
+			<div className="discover-separation"></div>
+
+			<ForYouChallengeTweetComponent
+				username={"godzila2.0"}
+				name={"godliza2.0"}
+				waspostedtime={"17h"}
+				sentence={"I found this challenge from @askralos, and I killed it 🤣🙃"}
+				challengeid={"456789"}
+				likes={"458"}
+				comments={"12"}
+				profilepicture={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/512px-LEGO_logo.svg.png"}
+			></ForYouChallengeTweetComponent>
+
+<div className="discover-separation"></div>
+
+<ForYouChallengeTweetComponent
+	username={"godzila2.0"}
+	name={"godliza2.0"}
+	waspostedtime={"17h"}
+	sentence={"I found this challenge from @askralos, and I killed it 🤣🙃"}
+	challengeid={"456789"}
+	likes={"458"}
+	comments={"12"}
+	profilepicture={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/512px-LEGO_logo.svg.png"}
+></ForYouChallengeTweetComponent>
+
+<div className="discover-separation"></div>
+
+			<ForYouChallengeTweetComponent
+				username={"godzila2.0"}
+				name={"godliza2.0"}
+				waspostedtime={"17h"}
+				sentence={"I found this challenge from @askralos, and I killed it 🤣🙃"}
+				challengeid={"456789"}
+				likes={"458"}
+				comments={"12"}
+				profilepicture={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/512px-LEGO_logo.svg.png"}
+			></ForYouChallengeTweetComponent>
+
+<div className="discover-separation"></div>
+
+<ForYouChallengeTweetComponent
+	username={"godzila2.0"}
+	name={"godliza2.0"}
+	waspostedtime={"17h"}
+	sentence={"I found this challenge from @askralos, and I killed it 🤣🙃"}
+	challengeid={"456789"}
+	likes={"458"}
+	comments={"12"}
+	profilepicture={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/512px-LEGO_logo.svg.png"}
+></ForYouChallengeTweetComponent>
+
+<div className="discover-separation"></div>
+
+			<ForYouChallengeTweetComponent
+				username={"godzila2.0"}
+				name={"godliza2.0"}
+				waspostedtime={"17h"}
+				sentence={"I found this challenge from @askralos, and I killed it 🤣🙃"}
+				challengeid={"456789"}
+				likes={"458"}
+				comments={"12"}
+				profilepicture={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/512px-LEGO_logo.svg.png"}
+			></ForYouChallengeTweetComponent>
+
+<div className="discover-separation"></div>
+
+<ForYouChallengeTweetComponent
+	username={"godzila2.0"}
+	name={"godliza2.0"}
+	waspostedtime={"17h"}
+	sentence={"I found this challenge from @askralos, and I killed it 🤣🙃"}
+	challengeid={"456789"}
+	likes={"458"}
+	comments={"12"}
+	profilepicture={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/512px-LEGO_logo.svg.png"}
+></ForYouChallengeTweetComponent>
+<div className="discover-separation"></div>
+
+			<ForYouChallengeTweetComponent
+				username={"godzila2.0"}
+				name={"godliza2.0"}
+				waspostedtime={"17h"}
+				sentence={"I found this challenge from @askralos, and I killed it 🤣🙃"}
+				challengeid={"456789"}
+				likes={"458"}
+				comments={"12"}
+				profilepicture={"https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/512px-LEGO_logo.svg.png"}
+			></ForYouChallengeTweetComponent>
+		</div>
+	);
 };
 
 export default Discover;
