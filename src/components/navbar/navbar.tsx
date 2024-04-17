acae3e7a1c2ca5e8583de28b8fbe75637986e97a
Thomasperge <thomaskauffmant74@gmail.com>
2024-04-17 22:47:35 +0200

    feat: adding navbar title

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IonTabBar, IonTabButton, IonIcon } from '@ionic/react';
 import { useLocation } from 'react-router-dom';
 import { homeSharp, receiptSharp, cogSharp, playSharp, peopleSharp } from 'ionicons/icons';
+import './navbar.css'
 
 const Navbar: React.FC = () => {
   const location = useLocation();
@@ -9,23 +10,28 @@ const Navbar: React.FC = () => {
   return (
     <IonTabBar slot="bottom">
       <IonTabButton tab="friends" href="/friends" selected={location.pathname === '/friends'}>
-        <IonIcon icon={peopleSharp} style={{ color: location.pathname === '/friends' ? 'var(--ion-color-600)' : '#C1C1C1'}} size="medium" />
+        <IonIcon icon={peopleSharp} style={{ color: location.pathname === '/friends' ? 'var(--ion-color-600)' : '#C1C1C1' }} size="medium" />
+        <span className='icon-text' style={{ color: location.pathname === '/friends' ? 'var(--ion-color-600)' : '#C1C1C1' }}>Profile</span>
       </IonTabButton>
 
       <IonTabButton tab="challenge" href="/challenge" selected={location.pathname === '/challenge'}>
-        <IonIcon icon={receiptSharp} style={{ color: location.pathname === '/challenge' ? 'var(--ion-color-600)' : '#C1C1C1'}} size="medium" />
+        <IonIcon icon={receiptSharp} style={{ color: location.pathname === '/challenge' ? 'var(--ion-color-600)' : '#C1C1C1' }} size="medium" />
+        <span className='icon-text' style={{ color: location.pathname === '/challenge' ? 'var(--ion-color-600)' : '#C1C1C1' }}>Challenge</span>
       </IonTabButton>
 
       <IonTabButton tab="home" href="/home" selected={location.pathname === '/home'}>
-        <IonIcon icon={homeSharp} style={{ color: location.pathname === '/home' ? 'var(--ion-color-600)' : '#C1C1C1'}} size="medium" />
+        <IonIcon icon={homeSharp} style={{ color: location.pathname === '/home' ? 'var(--ion-color-600)' : '#C1C1C1' }} size="medium" />
+        <span className='icon-text' style={{ color: location.pathname === '/home' ? 'var(--ion-color-600)' : '#C1C1C1' }}>Home</span>
       </IonTabButton>
 
       <IonTabButton tab="discover" href="/discover" selected={location.pathname === '/discover'}>
-        <IonIcon icon={playSharp} style={{ color: location.pathname === '/discover' ? 'var(--ion-color-600)' : '#C1C1C1'}} size="medium" />
+        <IonIcon icon={playSharp} style={{ color: location.pathname === '/discover' ? 'var(--ion-color-600)' : '#C1C1C1' }} size="medium" />
+        <span className='icon-text' style={{ color: location.pathname === '/discover' ? 'var(--ion-color-600)' : '#C1C1C1' }}>Foryou</span>
       </IonTabButton>
 
       <IonTabButton tab="settings" href="/settings" selected={location.pathname === '/settings'}>
-        <IonIcon icon={cogSharp} style={{ color: location.pathname === '/settings' ? 'var(--ion-color-600)' : '#C1C1C1'}} size="medium" />
+        <IonIcon icon={cogSharp} style={{ color: location.pathname === '/settings' ? 'var(--ion-color-600)' : '#C1C1C1' }} size="medium" />
+        <span className='icon-text' style={{ color: location.pathname === '/settings' ? 'var(--ion-color-600)' : '#C1C1C1' }}>Settings</span>
       </IonTabButton>
     </IonTabBar>
   );
