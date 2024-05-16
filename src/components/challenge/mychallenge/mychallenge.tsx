e232e528e71219cc1b0eb92ea98db1a82d5f41d5
thomasperge <thomaskauffmant74@gmail.com>
2024-05-16 02:29:09 +0200

    feat: adding challenge finish + spinner

--- src/components/challenge/mychallenge/mychallenge.tsx
+++ src/components/challenge/mychallenge/mychallenge.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { IonSpinner } from '@ionic/react';
+import { IonRefresher, IonRefresherContent, IonSpinner } from '@ionic/react';
 import ChallengeItemsComponent from '../../challengeitems/challengeitems.js';
 import HeadingComponent from '../../heading/heading.js';
 import ChallengeItemsInProgressComponent from '../../challengeitemsinprogress/challengeitemsinprogress.js';
@@ -9,94 +9,115 @@ const MyChallengeComponent: React.FC = () => {
   const [challenges, setChallenges] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchChallenges = async () => {
-      try {
-        const userId = localStorage.getItem('user_id');
+  const fetchData = async () => {
+    try {
+      const userId = localStorage.getItem('user_id');
 
-        const response = await fetch(`http://localhost:3000/challenge/user/${userId}`);
-        if (!response.ok) {
-          throw new Error('Une erreur est survenue lors de la récupération des données.');
-        }
-
-        const data = await response.json();
-        setChallenges(data);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
+      const response = await fetch(`http://localhost:3000/challenge/user/${userId}`);
+      if (!response.ok) {
+        throw new Error('Une erreur est survenue lors de la récupération des données.');
       }
-    };
 
-    fetchChallenges();
+      const data = await response.json();
+      console.log(data);
+      setChallenges(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
   }, []);
 
   if (loading) {
     return (
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '1.5rem 0' }}>
-        <IonSpinner name="crescent"/>
+        <IonSpinner name="crescent" />
       </div>
     );
   }
 
+  const handleRefresh = async (event: CustomEvent) => {
+    await fetchData();
+    event.detail.complete();
+  };
+
   return (
-    <div className='ion-padding-bottom'>
-      {/* Latest Challenges */}
-      <HeadingComponent
-        text="Latest Challenges"
-        fontSize="1.2rem"
-        fontWeight="600"
-        color="var(--ion-color-dark)"
-        padding="0 0 .5rem 0"
-      />
+    <>
+      <IonRefresher slot="fixed" onIonRefresh={handleRefresh}>
+        <IonRefresherContent />
+      </IonRefresher>
 
-      {/* Aucun challenge terminé */}
-      {challenges.every(({ completed }) => !completed) && (
-        <div style={{ color: "gray" }}>Aucun challenge terminé</div>
-      )}
-
-      {/* Challenges Terminés */}
-      <div className="column ion-margin-bottom" style={{ gap: ".5rem" }}>
-        {challenges.filter(({ completed }) => completed).map(({ _id, createdAt, hobbies, text, completed }) => (
-          <ChallengeItemsComponent
-            key={_id}
-            createdAt={createdAt}
-            categorie={hobbies}
-            challenge={text}
-            completed={completed}
-            image=""
+      {/* Affichage des données */}
+      {loading ? (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <IonSpinner name="crescent" />
+        </div>
+      ) : (
+        <div className='ion-padding-bottom'>
+          {/* Latest Challenges */}
+          <HeadingComponent
+            text="Latest Challenges"
+            fontSize="1.2rem"
+            fontWeight="600"
+            color="var(--ion-color-dark)"
+            padding="0 0 .5rem 0"
           />
-        ))}
-      </div>
 
-      {/* Challenges en Cours */}
-      <HeadingComponent
-        text="Challenges in progress"
-        fontSize="1.2rem"
-        fontWeight="600"
-        color="var(--ion-color-dark)"
-        padding="0 0 .5rem 0"
-      />
+          {/* Aucun challenge terminé */}
+          {challenges.every(({ completed }) => !completed) && (
+            <div style={{ color: "gray" }}>Aucun challenge terminé</div>
+          )}
 
-      {/* Aucun challenge en cours */}
-      {challenges.every(({ completed }) => completed) && (
-        <div style={{ color: "gray" }}>Aucun challenge en cours</div>
-      )}
+          {/* Challenges Terminés */}
+          <div className="column ion-margin-bottom" style={{ gap: ".5rem" }}>
+            {challenges.filter(({ completed }) => completed).map(({ challenge_joined_id, challenge_id, createdAt, hobbies, text, completed }) => (
+              <ChallengeItemsComponent
+                key={challenge_id}
+                _id={challenge_joined_id}
+                createdAt={createdAt}
+                categorie={hobbies}
+                challenge={text}
+                completed={completed}
+                image=""
+              />
+            ))}
+          </div>
 
-      {/* Challenges en Cours */}
-      <div className="column ion-margin-bottom" style={{ gap: ".5rem" }}>
-        {challenges.filter(({ completed }) => !completed).map(({ _id, createdAt, hobbies, text, completed }) => (
-          <ChallengeItemsInProgressComponent
-            key={_id}
-            createdAt={createdAt}
-            categorie={hobbies}
-            challenge={text}
-            completed={completed}
-            image=""
+          {/* Challenges en Cours */}
+          <HeadingComponent
+            text="Challenges in progress"
+            fontSize="1.2rem"
+            fontWeight="600"
+            color="var(--ion-color-dark)"
+            padding="0 0 .5rem 0"
           />
-        ))}
-      </div>
-    </div>
+
+          {/* Aucun challenge en cours */}
+          {challenges.every(({ completed }) => completed) && (
+            <div style={{ color: "gray" }}>Aucun challenge en cours</div>
+          )}
+
+          {/* Challenges en Cours */}
+          <div className="column ion-margin-bottom" style={{ gap: ".5rem" }}>
+            {challenges.filter(({ completed }) => !completed).map(({ challenge_joined_id, challenge_id, createdAt, hobbies, text, completed }) => (
+              <ChallengeItemsInProgressComponent
+                key={challenge_id}
+                _id={challenge_joined_id}
+                createdAt={createdAt}
+                categorie={hobbies}
+                challenge={text}
+                completed={completed}
+                image=""
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
